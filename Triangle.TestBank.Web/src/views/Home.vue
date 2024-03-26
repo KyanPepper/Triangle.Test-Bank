ca169e90d390f640f6d54a15ae2267a5630cc2d0
KyanPepper <kyankks1@gmail.com>
2024-03-26 01:53:02 -0700

    Exam list data

--- Triangle.TestBank.Web/src/views/Home.vue
+++ Triangle.TestBank.Web/src/views/Home.vue
@@ -1,9 +1,25 @@
 <template>
   <div>
-  
+    <v-card v-for="exam in examList" :key="exam.examId">
+      <v-card-title>{{ exam.name }}</v-card-title>
+      <v-card-text>{{ exam.pdfPath }}</v-card-text>
+    </v-card>
+    arqwe
   </div>
 </template>
 
 <script setup lang="ts">
+import { ExamApiClient } from '@/api-clients.g';
+import { Exam } from '@/models.g';
+let examList = ref<Exam[]>([]);
 
+onMounted(async () => {
+  console.log("Home page mounted");
+  let client = new ExamApiClient();
+  const response = client.$makeCaller("list", methods => methods.list());
+  await response();
+  examList.value = response.result as Exam[];
+  examList.value = toRaw(examList.value);
+  console.log(examList.value);
+});
 </script>
