e1191b88e479c7b82916a83929be91ff9c6f78c3
KyanPepper <kyankks1@gmail.com>
2024-03-27 00:50:46 -0700

    ExamCard setup

--- Triangle.TestBank.Web/src/views/Home.vue
+++ Triangle.TestBank.Web/src/views/Home.vue
@@ -1,25 +1,19 @@
 <template>
-  <div>
-    <v-card v-for="exam in examList" :key="exam.examId">
-      <v-card-title>{{ exam.name }}</v-card-title>
-      <v-card-text>{{ exam.pdfPath }}</v-card-text>
-    </v-card>
-    arqwe
-  </div>
+  <ExamCard />
 </template>
 
 <script setup lang="ts">
-import { ExamApiClient } from '@/api-clients.g';
-import { Exam } from '@/models.g';
-let examList = ref<Exam[]>([]);
+import { ref, onMounted } from "vue";
+import { ExamApiClient } from "@/api-clients.g";
+import { Exam } from "@/models.g";
+const examList = ref<Exam[]>([]);
 
 onMounted(async () => {
   console.log("Home page mounted");
-  let client = new ExamApiClient();
-  const response = client.$makeCaller("list", methods => methods.list());
+  const client = new ExamApiClient();
+  const response = client.$makeCaller("list", (methods) => methods.list());
   await response();
   examList.value = response.result as Exam[];
-  examList.value = toRaw(examList.value);
   console.log(examList.value);
 });
 </script>
