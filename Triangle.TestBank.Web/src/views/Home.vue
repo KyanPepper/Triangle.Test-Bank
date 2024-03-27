1802e03b3fdaf2206a19b9e7d8d4ba4d8db02e08
KyanPepper <kyankks1@gmail.com>
2024-03-27 03:11:08 -0700

    Downlaod functoinality for exam cards

--- Triangle.TestBank.Web/src/views/Home.vue
+++ Triangle.TestBank.Web/src/views/Home.vue
@@ -1,11 +1,24 @@
 <template>
-
-  <div>
-    <ExamCard v-for="exam in examList" :key="exam.examId" :name="exam.name" :term="TermToText(Number(exam.term))"
-      :subject="SubjectToText(Number(exam.subject))" :pdf="exam.pdfPath"></ExamCard>
+  <div class="exam-card-container">
+    <ExamCard
+      v-for="exam in examList"
+      :key="exam.examId"
+      :name="exam.name"
+      :term="TermToText(Number(exam.term))"
+      :subject="SubjectToText(Number(exam.subject))"
+      :pdf="exam.pdfPath"
+    ></ExamCard>
   </div>
-
 </template>
+
+<style scoped>
+.exam-card-container {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  gap: 20px;
+  padding: 30px;
+}
+</style>
 
 <script setup lang="ts">
 import { ref, onMounted } from "vue";
@@ -74,5 +87,4 @@ function TermToText(enumValue: number): string {
       return "Unknown";
   }
 }
-
 </script>
