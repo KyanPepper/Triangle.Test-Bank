85e25ab8a7d9ca3e382d089dc944fc58b12636aa
KyanPepper <kyankks1@gmail.com>
2024-03-28 01:10:41 -0700

    Typescipt v model for form

--- Triangle.TestBank.Web/src/views/Upload.vue
+++ Triangle.TestBank.Web/src/views/Upload.vue
@@ -1,5 +1,62 @@
 <template>
-    <h1>
-        Upload Exam
-    </h1>
-</template>
+    <VContainer class="">
+      <VForm class="h-full">
+        <VTextField
+          v-model="examState.name"
+          label="Exam Name"
+          required
+        />
+        <v-select
+          v-model="examState.subject"
+          label="Subject"
+          required
+        />
+        <v-select
+          v-model="examState.term"
+          label="Term"
+          required
+        />
+        <VFileInput
+          v-model="examState.pdf"
+          label="Exam File"
+          required
+        />
+        <VBtn color="primary" @click="uploadExam">
+          Upload Exam
+        </VBtn>
+      </VForm>
+    </VContainer>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent, ref } from 'vue';
+  
+  interface ExamModel {
+    name: string;
+    subject: number | null;
+    term: number | null;
+    pdf: any | null;
+  }
+  
+  export default defineComponent({
+    setup() {
+      const examState = ref<ExamModel>({
+        name: '',
+        subject: null,
+        term: null,
+        pdf: null
+      });
+  
+      const uploadExam = () => {
+        console.log('Upload exam clicked');
+        console.log(examState.value.pdf);
+      };
+  
+      return {
+        examState,
+        uploadExam
+      };
+    }
+  });
+  </script>
+  
